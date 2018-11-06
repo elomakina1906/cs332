3880cdc71098c1d81a6e84a2f1298b15ab041bbc
Alan <kumaritof@ya.ru>
2018-11-06 16:47:59 +0300

    security update

--- individual_2d/jarvis.py
+++ individual_2d/jarvis.py
@@ -3,64 +3,59 @@ from math import acos, sqrt
 
 class Jarvis():
 	def __init__(self, points, height):
-		self.height = height
-		self.points = points
-		self.result = []
-
+		self.__height = height
+		self.__points = points
+		self.__result = []
 
 	def __find_first(self):
 		by_x = lambda x: x[0]
 		by_y = lambda x: x[1]
-		self.points = sorted(self.points, key = by_x)
-		self.points = sorted(self.points, key = by_y, reverse = True)
-		first = self.points[0]
+		self.__points = sorted(self.__points, key = by_x)
+		self.__points = sorted(self.__points, key = by_y, reverse = True)
+		first = self.__points[0]
 		return first
 
-
 	def __line_lenght(self, a, b):
-		really_y = lambda y: self.height - y
+		really_y = lambda y: self.__height - y
 		d_x = b[0] - a[0]
 		d_y = really_y(b[1]) - really_y(a[1])
 		return sqrt(d_x * d_x + d_y * d_y) 
 
-
 	def __angle_between_lines(self, p):
-		n = len(self.result)
+		n = len(self.__result)
 		if n < 2:
-			b = self.result[0]
+			b = self.__result[0]
 			a = (b[0] - 1, b[1])
 		else:
-			a = self.result[n - 2]
-			b = self.result[n - 1]
+			a = self.__result[n - 2]
+			b = self.__result[n - 1]
 		ab = self.__line_lenght(a, b)
 		bp = self.__line_lenght(b, p)
 		ap = self.__line_lenght(a, p)
 		cos_value = (bp * bp + ab * ab - ap * ap) / (2 * ab * bp)
 		return acos(cos_value)
 
-
 	def __id_next_point(self):
-		n = len(self.points)
-		l = len(self.result)
+		n = len(self.__points)
+		l = len(self.__result)
 		idm, mc = 0, -180
 		for i in range(0, n):
-			if (self.result[l-1] != self.points[i]):
-				c = self.__angle_between_lines(self.points[i])
+			if (self.__result[l-1] != self.__points[i]):
+				c = self.__angle_between_lines(self.__points[i])
 				if c > mc:
 					idm = i
 					mc = c
 		return idm
 
-
 	def solver(self):
 		first = self.__find_first()
-		self.result.append(first)
+		self.__result.append(first)
 		while True:
 			id = self.__id_next_point()
 			if id == 0:
 				break
 			else:
-				self.result.append(self.points[id])
-				del self.points[id]
-		self.result.append(first)
-		return self.result
+				self.__result.append(self.__points[id])
+				del self.__points[id]
+		self.__result.append(first)
+		return self.__result
--- individual_2d/winclass.py
+++ individual_2d/winclass.py
@@ -8,56 +8,50 @@ HEIGHT = 500
 class Main(tk.Frame):
 	def __init__(self, parent):
 		super().__init__()
-		self.parent = parent
+		self.__parent = parent
 		self.__init_ui()
 
-
 	def __init_ui(self):
-		self.parent.title("Jarvis")
-		self.points = []
-		self.vertexes = []
+		self.__parent.title("Jarvis")
+		self.__points = []
+		self.__vertexes = []
 
-		self.doit = tk.Button(text="Jarvis")
-		self.doit.bind("<Button-1>", self.__do_js)
-		self.doit.grid(row=1, column=0)
+		self.__doit = tk.Button(text="Jarvis")
+		self.__doit.bind("<Button-1>", self.__do_js)
+		self.__doit.grid(row=1, column=0)
 		
-		self.clear = tk.Button(text="Clear")
-		self.clear.bind("<Button-1>", self.__clear_canvas)
-		self.clear.grid(row=1, column=1)
+		self.__clear = tk.Button(text="Clear")
+		self.__clear.bind("<Button-1>", self.__clear_canvas)
+		self.__clear.grid(row=1, column=1)
 
-		self.canvas = tk.Canvas(width=WIDTH, height=HEIGHT, bg='white')
-		self.canvas.bind("<Button-1>", self.__add_point)
-		self.canvas.grid(row=0, column=0, columnspan=2)
-		
+		self.__canvas = tk.Canvas(width=WIDTH, height=HEIGHT, bg='white')
+		self.__canvas.bind("<Button-1>", self.__add_point)
+		self.__canvas.grid(row=0, column=0, columnspan=2)		
 
 	def __draw_points(self):
-		for point in self.points:
+		for point in self.__points:
 			x1, y1 = point[0] - 1, point[1] - 1
 			x2, y2 = point[0] + 1, point[1] + 1
-			self.canvas.create_oval(x1, y1, x2, y2)
-
+			self.__canvas.create_oval(x1, y1, x2, y2)
 
 	def __draw_lines(self):
-		self.canvas.create_line(self.vertexes)
-
+		self.__canvas.create_line(self.__vertexes)
 
 	def __add_point(self, event):
-		self.points.append((event.x, event.y))
+		self.__points.append((event.x, event.y))
 		self.__draw_points()
-
 
 	def __do_js(self, event):
-		self.vertexes.clear()
-		self.canvas.delete("all")
+		self.__vertexes.clear()
+		self.__canvas.delete("all")
 		self.__draw_points()
-		if len(self.points) < 3:
+		if len(self.__points) < 3:
 			return
-		js = Jarvis(self.points, self.canvas.winfo_height())
-		self.vertexes = js.solver()
+		js = Jarvis(self.__points, self.__canvas.winfo_height())
+		self.__vertexes = js.solver()
 		self.__draw_lines()
 
-
 	def __clear_canvas(self, event):
-		self.points.clear()
-		self.vertexes.clear()
-		self.canvas.delete("all")
+		self.__points.clear()
+		self.__vertexes.clear()
+		self.__canvas.delete("all")
